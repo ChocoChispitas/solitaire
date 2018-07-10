89aa2413c3817c2d9aff74527b114a91b3d5d218
ChocoChispitas <33879699+ChocoChispitas@users.noreply.github.com>
2018-07-10 12:12:20 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var cards = {
+    rank: "",
+    suit: "",
+    colour: ""
+};
+
+var rank = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+var suit = ["Clubs", "Diamonds", "Hearts", "Spades"];
+var colour = ["red", "black"];
